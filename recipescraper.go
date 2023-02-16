e0e6ad113455049a6aea1c00d999fc735a76f1a1
Jason Gould <15948550+jasongould02@users.noreply.github.com>
2023-02-16 09:18:49 -0500

    hid some print statements, added URL to RecipeMeta, adjusted some spacing in structs

--- recipescraper.go
+++ recipescraper.go
@@ -1,47 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"strings"
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"encoding/json"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gorilla/mux"
 )
 
 type RecipeIngredient struct {
-	Name	string	`json:"name"`
-	Amount	string	`json:"amount"`
-	Unit	string	`json:"unit"`
+	Name   string `json:"name"`
+	Amount string `json:"amount"`
+	Unit   string `json:"unit"`
 }
 
 type RecipeInstruction struct {
-	Instruction string	`json:"instruction"`
-	Number		int		`json:"number"`
+	Instruction string `json:"instruction"`
+	Number      int    `json:"number"`
 }
 
 type RecipeNutrition struct {
-	Name	string	`json:"name"`
-	Amount	string	`json:"amount"`
-	Unit	string	`json:"unit"`
+	Name   string `json:"name"`
+	Amount string `json:"amount"`
+	Unit   string `json:"unit"`
 }
 
 type RecipeMeta struct {
-	Servings		string	`json:"servings"`
-	ServingsUnit	string	`json:"servingsUnit"`
-	Cuisine			string	`json:"cuisine"`
-	Course			string	`json:"course"`
-	Author			string	`json:"author"`
-	PrepTime		string	`json:"prepTime"`
-	PrepTimeUnit	string	`json:"prepTimeUnit"`
-	CookTime		string	`json:"cookTime"`
-	CookTimeUnit	string	`json:"cookTimeUnit"`
-	TotalTime		string	`json:"totalTime"`
-	Summary			string	`json:"summary"`
-	Title			string  `json:"title"`
+	Servings     string `json:"servings"`
+	ServingsUnit string `json:"servingsUnit"`
+	Cuisine      string `json:"cuisine"`
+	Course       string `json:"course"`
+	Author       string `json:"author"`
+	PrepTime     string `json:"prepTime"`
+	PrepTimeUnit string `json:"prepTimeUnit"`
+	CookTime     string `json:"cookTime"`
+	CookTimeUnit string `json:"cookTimeUnit"`
+	TotalTime    string `json:"totalTime"`
+	Summary      string `json:"summary"`
+	Title        string `json:"title"`
+	Url          string `json:"url"`
 }
 
 func scrapeIngredients(url string) []*RecipeIngredient {
@@ -63,15 +63,15 @@ func scrapeIngredients(url string) []*RecipeIngredient {
 	var ingredientList []*RecipeIngredient
 	doc.Find(".wprm-recipe-ingredient-group .wprm-recipe-ingredient").Each(
 		func(i int, selection *goquery.Selection) {
-		   amount := selection.Find(".wprm-recipe-ingredient-amount").Text()
-           unit := selection.Find(".wprm-recipe-ingredient-unit").Text()
-           name := selection.Find(".wprm-recipe-ingredient-name").Text()
-           if amount == "" { // probably an optional ingredient
-			   log.Println("Ingredient found but has no amount, finish check for optional ingredients")
-           }
-		   log.Printf("Ingredient:%s\tAmount:%s\tUnit:%s\n", name, amount, unit)
-           ingredientList = append(ingredientList, &RecipeIngredient{Name: name, Amount: amount, Unit: unit})
-	});
+			amount := selection.Find(".wprm-recipe-ingredient-amount").Text()
+			unit := selection.Find(".wprm-recipe-ingredient-unit").Text()
+			name := selection.Find(".wprm-recipe-ingredient-name").Text()
+			if amount == "" { // probably an optional ingredient
+				log.Println("Ingredient found but has no amount, finish check for optional ingredients")
+			}
+			//log.Printf("Ingredient:%s\tAmount:%s\tUnit:%s\n", name, amount, unit)
+			ingredientList = append(ingredientList, &RecipeIngredient{Name: name, Amount: amount, Unit: unit})
+		})
 	return ingredientList
 }
 
@@ -96,7 +96,7 @@ func scrapeInstructions(url string) []*RecipeInstruction {
 		func(i int, selection *goquery.Selection) {
 			instruction := selection.Find(".wprm-recipe-instruction-text").Text()
 			instructionList = append(instructionList, &RecipeInstruction{Instruction: instruction, Number: i})
-	});
+		})
 	return instructionList
 }
 
@@ -119,12 +119,12 @@ func scrapeNutrition(url string) []*RecipeNutrition {
 	var nutritionList []*RecipeNutrition
 	doc.Find(".wprm-nutrition-label-text-nutrition-container").Each(func(i int, selection *goquery.Selection) {
 		amount := selection.Find(".wprm-nutrition-label-text-nutrition-value").Text()
-		unit   := selection.Find(".wprm-nutrition-label-text-nutrition-unit").Text()
-		name   := selection.Find(".wprm-nutrition-label-text-nutrition-label").Text()
+		unit := selection.Find(".wprm-nutrition-label-text-nutrition-unit").Text()
+		name := selection.Find(".wprm-nutrition-label-text-nutrition-label").Text()
 
-		log.Printf("Nutrition Label: %s\tAmount:%s\tUnit:%s\n", name, amount, unit)
+		//log.Printf("Nutrition Label: %s\tAmount:%s\tUnit:%s\n", name, amount, unit)
 		nutritionList = append(nutritionList, &RecipeNutrition{Name: name, Amount: amount, Unit: unit})
-	});
+	})
 	return nutritionList
 }
 
@@ -149,7 +149,7 @@ func scrapeMeta(url string) RecipeMeta {
 	cuisine := doc.Find(".wprm-recipe-cuisine").Text()
 	course := doc.Find(".wprm-recipe-course").Text()
 	author := doc.Find(".wprm-recipe-author").Text()
-	log.Printf("Servings:%s %s\tCuisine:%s\tCourse:%s\tAuthor:%s\n", servings, servingsUnit, cuisine, course, author)
+	//log.Printf("Servings:%s %s\tCuisine:%s\tCourse:%s\tAuthor:%s\n", servings, servingsUnit, cuisine, course, author)
 
 	prepTime := doc.Find(".wprm-recipe-prep_time").Text()
 	prepTimeUnit := doc.Find(".wprm-recipe-prep_time-unit").Text()
@@ -162,26 +162,27 @@ func scrapeMeta(url string) RecipeMeta {
 		totalTime += " " + selection.Text()
 	})
 	totalTime = strings.TrimSpace(totalTime)
-	log.Printf("Prep Time:%s %s\tCook Time:%s %s\tTotal Time:%s\n", prepTime, prepTimeUnit, cookTime, cookTimeUnit, totalTime)
+	//log.Printf("Prep Time:%s %s\tCook Time:%s %s\tTotal Time:%s\n", prepTime, prepTimeUnit, cookTime, cookTimeUnit, totalTime)
 
 	summary := doc.Find(".wprm-recipe-summary").Text()
-	log.Printf("Summary:%s\n", summary)
+	//log.Printf("Summary:%s\n", summary)
 
 	title := doc.Find(".breadcrumb_last").Text()
-	log.Printf("Title:%s\n", title)
+	//log.Printf("Title:%s\n", title)
 
-	recipeMeta := RecipeMeta{Servings: servings, ServingsUnit: servingsUnit, Cuisine: cuisine, Course: course,
-							 Author: author, PrepTime: prepTime, PrepTimeUnit: prepTimeUnit,
-							 CookTime: cookTime, CookTimeUnit: cookTimeUnit, TotalTime: totalTime, Summary: summary, Title: title}
+	recipeMeta := RecipeMeta{Servings: servings, ServingsUnit: servingsUnit, Cuisine: cuisine,
+		Course: course, Author: author, PrepTime: prepTime,
+		PrepTimeUnit: prepTimeUnit, CookTime: cookTime, CookTimeUnit: cookTimeUnit,
+		TotalTime: totalTime, Summary: summary, Title: title, Url: url}
 
 	return recipeMeta
 }
 
 type Recipe struct {
-	Metadata RecipeMeta						`json:"Metadata"`
-	NutritionList []*RecipeNutrition		`json:"Nutrition"`
-	InstructionList []*RecipeInstruction	`json:"Instruction"`
-	IngredientList []*RecipeIngredient		`json:"Ingredient"`
+	Metadata        RecipeMeta           `json:"Metadata"`
+	NutritionList   []*RecipeNutrition   `json:"Nutrition"`
+	InstructionList []*RecipeInstruction `json:"Instruction"`
+	IngredientList  []*RecipeIngredient  `json:"Ingredient"`
 }
 
 func (r *Recipe) EncodeRecipe() []byte {
@@ -233,7 +234,7 @@ func (r Recipe) encodeIngredientList(list []*RecipeIngredient) []byte {
 }
 
 type GetRecipeRequest struct {
-	Url string	`json:"URL"`
+	Url string `json:"URL"`
 }
 
 func RecipeHandler(w http.ResponseWriter, r *http.Request) {
@@ -241,25 +242,18 @@ func RecipeHandler(w http.ResponseWriter, r *http.Request) {
 	var data Recipe
 	//var decodedString string
 
-	fmt.Println("----------------------------")
+	/*fmt.Println("----------------------------")
 	temp, _ := httputil.DumpRequest(r, true)
 	fmt.Println(string(temp))
-	fmt.Println("----------------------------")
-
+	fmt.Println("----------------------------")*/
 
 	decoder := json.NewDecoder(r.Body)
-	decoder.DisallowUnknownFields()
-	//err := decoder.Decode(&decodedString)
+	decoder.DisallowUnknownFields() //err := decoder.Decode(&decodedString)
 	err := decoder.Decode(&recipeRequest)
 	fmt.Println("recipeRequest is now:", recipeRequest.Url)
 	if err != nil {
 		log.Printf("Error decoding JSON: %s\n", err)
 	}
-	/*fmt.Println("Remove this print statement")
-	err = json.Unmarshal([]byte(decodedString), &recipeRequest)
-	if err != nil {
-		log.Printf("Error unmarshalling JSON: %s\n", err)
-	}*/
 
 	log.Printf("Received recipe URL is: %s\n", recipeRequest.Url)
 
@@ -278,7 +272,7 @@ func main() {
 	//fmt.Println(args)
 	//args := os.Args[1:]
 	//if args == nil || args[0] == "" {
-		//return
+	//return
 	//}
 
 	fmt.Println("Starting Recipe-Scraper Server.")
@@ -289,6 +283,6 @@ func main() {
 	err := http.ListenAndServe("localhost:4000", mux)
 	//log.Fatal(err)
 	if err != nil {
-		log.Printf("Error: %s\n", err);
+		log.Printf("Error: %s\n", err)
 	}
 }
